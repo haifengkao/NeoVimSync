ec37f928aac1100233dcc0e96cb36e96e03272dc
Hai Feng <haifeng@cocoaspice.in>
2019-09-17 20:47:03 +0800

    mostly done

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# whatfile README
+# NeoVimSync README
 
-Writes the filename of the current file being modified to a specified file location.
+add the edited files to the nvim recent file list
 
-![Demo](./demo.gif)
 ## Extension Settings
 
 This extension contributes the following settings:
 
-* `whatFile.fileLocation`: File to write to
-* `whatFile.enable`: enable/disable this extension
+* `NeoVimSync.nvimLocation`: the location of nvim executable
+* `NeoVimSync.enable`: enable/disable this extension
+* `NeoVimSync.loggingEnabled`: enable/disable logging to developer tools
 
 ## Known Issues
 
@@ -20,8 +20,3 @@ Inital release
 
 ### 0.0.1
 - Initial release
-### 0.0.2
-...
-### 0.0.3
-
-- Fixed bug where if the file that is being written to was open in vscode it would set the filename to itself
--- src/extension.ts
+++ src/extension.ts
@@ -5,72 +5,81 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as cp from 'child_process';
 
+var neoVimSyncLoggingEnabled = false;
+
 export function dumpInConsole(msg: string) {
-  console.log(msg);
+    if (neoVimSyncLoggingEnabled) {
+        console.log(msg);
+    }
 }
 
 let stdout: string, stderr: string, error: Error | null;
 ///managed build now only support to invoke on save
-export function touchFile(
-  swiftBinPath: string,
-  params: string[]
-) {
-  stdout = "";
-  stderr = "";
-  error = null;
-  const sb = cp.spawn(swiftBinPath, params, { shell: true });
-  sb.stdout.on("data", data => {
-    stdout += data;
-    dumpInConsole("" + data);
-  });
-  sb.stderr.on("data", data => {
-    stderr += data;
-    dumpInConsole("" + data);
-  });
-  sb.on("error", function(err: Error) {
-    dumpInConsole("***swift build command error*** " + err.message);
-    error = err;
-  });
+export function touchFile(swiftBinPath: string, params: string[]) {
+    stdout = "";
+    stderr = "";
+    error = null;
+    const sb = cp.spawn(swiftBinPath, params, { shell: true });
+    sb.stdout.on("out", data => {
+        stdout += data;
+        dumpInConsole("***nvim sync data*** " + data);
+    });
+    sb.stderr.on("data", data => {
+        stderr += data;
+        dumpInConsole("***nvim sync err***" + data);
+    });
+    sb.on("error", function(err: Error) {
+        dumpInConsole("***nvim sync error*** " + err.message);
+        error = err;
+    });
 
-  sb.on("exit", function(code, signal) {
-    dumpInConsole(`***swift build command exited*** code: ${code}, signal: ${signal}`);
-  });
+    sb.on("exit", function(code, signal) {
+        dumpInConsole(`***nvim sync exited*** code: ${code}, signal: ${signal}`);
+    });
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate({ subscriptions }: vscode.ExtensionContext) {
 
-	let lastChangedFile: string = "";
+    let lastChangedFile: string = "";
 
+    // When a file changes get the name from `filepath`
+    const thing = vscode.workspace.onDidChangeTextDocument((filepath) => {
+        // if enabled is not defined or false then don't do anything...
+        const isEnabled: boolean | undefined = vscode.workspace.getConfiguration().get("NeoVimSync.enabled");
+        const nvimLocation: string | undefined = vscode.workspace.getConfiguration().get("NeoVimSync.nvimLocation");
+        const loggingEnabled: boolean | undefined  = vscode.workspace.getConfiguration().get("NeoVimSync.loggingEnabled");
+        if (loggingEnabled != undefined) {
+            neoVimSyncLoggingEnabled = loggingEnabled
+        }
 
+        dumpInConsole("NeoVimSync didChange: " + filepath);
 
-	// When a file changes get the name from `filepath`
-	const thing = vscode.workspace.onDidChangeTextDocument((filepath) => {
-		// if enabled is not defined or false then don't do anything...
-		const isEnabled: boolean | undefined = vscode.workspace.getConfiguration().get("NeoVimSync.enabled");
-		const nvimLocation: string | undefined = vscode.workspace.getConfiguration().get("NeoVimSync.nvimLocation");
+        if (!isEnabled) {
+            dumpInConsole("NeoVimSync disabled");
+            return;
+        } else if (!nvimLocation) {
+            vscode.window.showInformationMessage("No nvim location in settings: Set `NeoVimSync.nvimLocation`");
+            return;
+        }
 
-		if (!isEnabled) {
-			return;
-		} else if (!nvimLocation) {
-			vscode.window.showInformationMessage("No file location in settings: Set `whatFile.nvimLocation`");
-			return;
-		}
+        // Get file name
+        const splitFilePath = filepath.document.fileName.split("\\");
+        const fileName = splitFilePath[splitFilePath.length-1];
+        
+        // If we're not equal to what was stored last time save name of the file.
+        if (fileName !== lastChangedFile) {
+            dumpInConsole("NeoVimSync touch: " + fileName);
+            touchFile("/usr/local/bin/nvim", ["--noplugin", "--headless", "+q!", fileName]);
+            lastChangedFile = fileName;
+        }
+    });
 
-		// Get file name
-		const splitFilePath = filepath.document.fileName.split("\\");
-		const fileName = splitFilePath[splitFilePath.length-1];
-		
-		// If we're not equal to what was stored last time save name of the file.
-		if (fileName !== lastChangedFile) {
-      touchFile("/usr/local/bin/nvim", [" --headless", "q!", fileName]);
-			lastChangedFile = fileName;
-		}
-	});
-
-	subscriptions.push(thing);
+    subscriptions.push(thing);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    dumpInConsole("NeoVimSync deactivate");
+}
